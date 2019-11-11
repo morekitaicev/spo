7df1df39479a35a95ad5132243b53165ee065215
morekitaicev <36542961+morekitaicev@users.noreply.github.com>
2019-11-12 01:17:00 +0300

    Add files via upload

---
+++ election/election_main.py
@@ -0,0 +1,240 @@
+from __future__ import print_function
+import requests, bs4
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open('election_final.txt', 'w')
+
+o = open('election_data.csv', 'w')
+u = open('beglov_data.csv', 'w')
+
+
+pages_to_parse = ['http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&root=1&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217417&type=222']
+
+table = requests.get('http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&root=1&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217417&type=222')
+soup = bs4.BeautifulSoup(table.text, "html.parser")
+collect = soup.find('body')
+b_tags_on_page = collect.findAll('b')
+
+start_table = []
+
+for x in b_tags_on_page:
+    start_table.append(str(x))
+
+
+for i in range(len(start_table)):
+    start_table[i] = start_table[i].replace('<b>', '')
+    start_table[i] = start_table[i].replace('</b>', '')
+    start_table[i] = start_table[i].replace('\xa0', '0')
+
+result_table = []
+for i in range(15):
+    result_table.append(start_table[4+i])
+del start_table[0:19]
+
+
+TIK = {}
+for i in range(31):
+    TIK[i] = []
+
+count_big_table = 0
+big_table = np.zeros((14, 31))
+
+
+for i in range(14):
+    for j in range(31):
+        big_table[i,j] = start_table[count_big_table]
+        count_big_table += 1
+
+print(big_table)
+
+np.set_printoptions(suppress=True)
+
+
+for i in range(31):
+    link = str(419+i)
+    pages_to_parse = ['http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217'+link+'&type=222']
+    table = requests.get('http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217'+link+'&type=222')
+    soup = bs4.BeautifulSoup(table.text, "html.parser")
+    collect = soup.find('body')
+    b_tags_on_page = collect.findAll('b')
+    start_table = []
+    for x in b_tags_on_page:
+      start_table.append(str(x))
+    for j in range(len(start_table)):
+        start_table[j] = start_table[j].replace('<b>', '')
+        start_table[j] = start_table[j].replace('</b>', '')
+        start_table[j] = start_table[j].replace('\xa0', '0')
+    del start_table[0:19]
+    number = len(start_table) // 14
+    TIK[i] = np.zeros((15, number))
+    count_big_table = 0
+    for r in range(14):
+        for j in range(number):
+            TIK[i][r, j] = start_table[count_big_table]
+            TIK[i] = np.array(TIK[i])
+            count_big_table += 1
+
+
+TIK[30] = np.zeros((14, 1))
+for i in range(14):
+    TIK[30][i,0] = big_table [i, 30]
+
+UIK = {}
+for i in range(31):
+    UIK[i] = []
+
+
+
+for i in range(30):
+    link = str(419+i)
+    pages_to_parse = ['http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217'+link+'&type=222']
+    table = requests.get('http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217'+link+'&type=222')
+    soup = bs4.BeautifulSoup(table.text, "html.parser")
+    collect = soup.find('body')
+    nobr_tags_on_page = collect.findAll('nobr')
+    uik_start_table = []
+    for x in nobr_tags_on_page:
+      uik_start_table.append(str(x))
+    for j in range(len(uik_start_table)):
+        uik_start_table[j] = uik_start_table[j].replace('<b>', '')
+        uik_start_table[j] = uik_start_table[j].replace('<nobr>', '')
+        uik_start_table[j] = uik_start_table[j].replace('</b>', '')
+        uik_start_table[j] = uik_start_table[j].replace('</nobr>', '')
+        uik_start_table[j] = uik_start_table[j].replace('\xa0', '0')
+    for p in range(len(uik_start_table)):
+        if  uik_start_table[p][0] == 'У':
+            UIK[i].append(uik_start_table[p])
+
+
+for i in range(30):
+    for j in range(TIK[i].shape[1]):
+        TIK[i][14,j] = int(''.join(filter(str.isdigit, UIK[i][j])))
+
+
+vibros = []
+
+for j in range(30):
+    y_axis = []
+    for i in range(TIK[j].shape[1]):
+        y_axis.append((TIK[j][2, i] + TIK[j][3, i]) / TIK[j][0, i])
+    sd = np.std(y_axis)
+    sr = np.mean(y_axis)
+    for i in range(TIK[j].shape[1]):
+        if ((TIK[j][2, i] + TIK[j][3, i]) / TIK[j][0, i]) - sr >= 2*sd:
+            vibros.append(TIK[j][14,i])
+
+f.write(str(vibros))
+
+
+x_axis = []
+y_axis = []
+y_axis_beglov = []
+
+fig = plt.figure()
+
+for j in range(31):
+    x_axis = []
+    y_axis = []
+    y_axis_beglov = []
+    for i in range(TIK[j].shape[1]):
+        x_axis.append(1 + i)
+        y_axis.append((TIK[j][2,i]+TIK[j][3,i]) / TIK[j][0,i])
+        y_axis_beglov.append(TIK[j][12,i] / TIK[j][0,i])
+    plt.subplot(7, 5, j+1)
+    plt.bar(x_axis, y_axis)
+    plt.bar(x_axis, y_axis_beglov)
+    str_TIK = 'TIK ' + str(j+1)
+    yavka_TIK = str((big_table[2,j] + big_table[3,j]) / big_table[0,j])
+    beglov_TIK = str(big_table[12,j] / (big_table[2,j] + big_table[3,j]))
+    if j < 30:
+        o.write(str_TIK + ';' + yavka_TIK + '\n')
+        u.write(str_TIK + ';' + beglov_TIK + '\n')
+    plt.title(str_TIK)
+
+plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.25, hspace=0.7)
+plt.show()
+
+x_axis = []
+y_axis = []
+
+for j in range(31):
+    for i in range(TIK[j].shape[1]):
+        x_axis.append(TIK[j][0,i])
+        y_axis.append((TIK[j][2,i]+TIK[j][3,i]) / TIK[j][0,i])
+
+fig,axs = plt.subplots(1, 2)
+axs[0].scatter(x_axis, y_axis)
+axs[0].set_xlabel('Количество избирателей')
+axs[0].set_ylabel('Явка')
+fig.suptitle('Зависимость явки от количества избирателей')
+axs[1].bar(x_axis, y_axis)
+axs[1].set_xlabel('Количество избирателей')
+axs[1].set_ylabel('Явка')
+plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.25, hspace=0.7)
+plt.show()
+
+
+x_axis = []
+y_axis = []
+
+for j in range(31):
+        x_axis.append(TIK[j].shape[1])
+        y_axis.append((big_table[2,j]+big_table[3,j]) / big_table[0,j])
+
+fig,axs = plt.subplots(1, 2)
+axs[0].scatter(x_axis, y_axis)
+axs[0].set_xlabel('Количество УИКов')
+axs[0].set_ylabel('Явка')
+fig.suptitle('Зависимость явки от количества избирателей')
+axs[1].bar(x_axis, y_axis)
+axs[1].set_xlabel('Количество УИКов')
+axs[1].set_ylabel('Явка')
+plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.25, hspace=0.7)
+plt.show()
+
+x_axis = []
+x_axis_percent = []
+y_axis_beglov = []
+y_axis_amosov = []
+y_axis_tikhonova = []
+
+for j in range(31):
+    for i in range(TIK[j].shape[1]):
+        x_axis_percent.append((TIK[j][2,i]+TIK[j][3,i]) / TIK[j][0,i])
+        x_axis.append(TIK[j][2,i]+TIK[j][3,i])
+        y_axis_beglov.append(TIK[j][12,i] / TIK[j][8,i])
+        y_axis_amosov.append(TIK[j][11, i] / TIK[j][8, i])
+        y_axis_tikhonova.append(TIK[j][13, i] / TIK[j][8, i])
+
+fig,axs = plt.subplots(2, 3)
+axs[0,0].scatter(x_axis, y_axis_beglov)
+axs[0,0].set_xlabel('Явка')
+axs[0,0].set_ylabel('Количество голосов в %')
+axs[0,0].set_title('Беглов')
+fig.suptitle('Зависимость количества голосов от явки')
+axs[0,1].scatter(x_axis, y_axis_amosov)
+axs[0,1].set_xlabel('Явка')
+axs[0,1].set_ylabel('Количество голосов в %')
+axs[0,1].set_title('Амосов')
+axs[0,2].scatter(x_axis, y_axis_tikhonova)
+axs[0,2].set_xlabel('Явка')
+axs[0,2].set_ylabel('Количество голосов в %')
+axs[0,2].set_title('Тихонова')
+
+
+axs[1,0].scatter(x_axis_percent, y_axis_beglov)
+axs[1,0].set_xlabel('Явка в процентах %')
+axs[1,0].set_ylabel('Количество голосов в %')
+axs[1,0].set_title('Беглов')
+fig.suptitle('Зависимость количества голосов от явки')
+axs[1,1].scatter(x_axis_percent, y_axis_amosov)
+axs[1,1].set_xlabel('Явка в процентах %')
+axs[1,1].set_ylabel('Количество голосов в %')
+axs[1,1].set_title('Амосов')
+axs[1,2].scatter(x_axis_percent, y_axis_tikhonova)
+axs[1,2].set_xlabel('Явка в процентах %')
+axs[1,2].set_ylabel('Количество голосов в %')
+axs[1,2].set_title('Тихонова')
+plt.subplots_adjust(left=0.05, bottom=0.10, right=0.95, top=0.90, wspace=0.25, hspace=0.7)
+plt.show()
---
+++ election/election_visualisation.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import geopandas as gpd
+import json
+import folium
+
+def json_to_geojson(data, districts):
+    assert type(data) == list
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "name": district,
+                    "coordinates": [[[d["lon"], d["lat"]] for d in data if d['name'] == district]],
+                },
+                "properties": {'name': district},
+
+            } for district in districts]
+    }
+
+    return geojson
+
+with open('district_coords.txt', 'r') as fp:
+    district_coords = json.loads(fp.read())
+
+# get a list of districts
+districts = list(set([district_coord['name'] for district_coord in district_coords]))
+
+# create a geojson
+geojson = json_to_geojson(district_coords, districts)
+
+gdf = gpd.GeoDataFrame.from_features(geojson)
+gdf['centroid_lon'] = gdf['geometry'].centroid.x
+gdf['centroid_lat'] = gdf['geometry'].centroid.y
+gdf.crs = {'init' :'epsg:4326'}
+
+
+yavka = pd.read_csv('election_data.csv', sep=';')
+yavka.columns = ['TIK', 'percent']
+gdf_yavka = gdf.merge(yavka, left_on='name', right_on='TIK')
+
+yavka_beglov = pd.read_csv('beglov_data.csv', sep=';')
+yavka_beglov.columns = ['TIK', 'beglov']
+gdf_beglov = gdf.merge(yavka_beglov, left_on='name', right_on='TIK')
+
+m = folium.Map(location=[59.9375, 30.3066], zoom_start=11)
+
+## add chloropleth layer
+m.choropleth(
+    geo_data=geojson,
+    name='Yavka percent',
+    data=gdf_yavka,
+    columns=['name', 'percent'],
+    fill_color='BuGn',
+    key_on='feature.geometry.name',
+    legend_name='Yavka percent'
+)
+
+fg = folium.FeatureGroup(name='TIK Info')
+for lat, lon, val, name in zip(gdf_yavka['centroid_lat'].tolist(), gdf_yavka['centroid_lon'].tolist(), gdf_yavka['percent'].tolist(), gdf_yavka['name'].tolist()):
+    html = f"""
+    <h2>Saint Petersburg's {name}<\h2><br>
+    <h4>Yavka: {float(round(val,4))} % <\h4>
+    """
+    fg.add_child(folium.Marker(location=[lat, lon], popup=html))
+
+m.add_child(fg)
+
+# enable layers to be turned in or out
+folium.LayerControl().add_to(m)
+
+# save it
+m.save('yavka_percent.html')
+
+
+
+t = folium.Map(location=[59.9375, 30.3066], zoom_start=11)
+
+## add chloropleth layer
+t.choropleth(
+    geo_data=geojson,
+    name='Yavka percent',
+    data=gdf_beglov,
+    columns=['name', 'beglov'],
+    fill_color='OrRd',
+    key_on='feature.geometry.name',
+    legend_name='Beglov percent'
+)
+
+tg = folium.FeatureGroup(name='Beglov Info')
+for lat, lon, val, name in zip(gdf_beglov['centroid_lat'].tolist(), gdf_beglov['centroid_lon'].tolist(), gdf_beglov['beglov'].tolist(), gdf_beglov['name'].tolist()):
+    html = f"""
+    <h2>Saint Petersburg's {name}<\h2><br>
+    <h4>Yavka: {float(round(val,4))} % <\h4>
+    """
+    tg.add_child(folium.Marker(location=[lat, lon], popup=html))
+
+t.add_child(tg)
+
+# enable layers to be turned in or out
+folium.LayerControl().add_to(t)
+
+# save it
+t.save('beglov_percent.html')
